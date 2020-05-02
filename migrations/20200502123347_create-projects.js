c847e603c29de6faf3e655b236f906ec5d8ff7ee
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-05-02 13:09:43 -0400

    TablesCreated

--- migrations/20200502123347_create-projects.js
+++ migrations/20200502123347_create-projects.js
@@ -1,8 +1,44 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('Projects', tbl => {
+        tbl.increments();
+        tbl.string('ProjectName')
+        .notNullable();
+        tbl.string("ProjectDescription")
+        .notNullable();
+        tbl.boolean('Completed');
+    })
+    .createTable('Tasks', tbl => {
+        tbl.increments();
+        tbl.string('TaskDescription')
+        .notNullable();
+        tbl.string('notes');
+        tbl.boolean('Completed');
+        tbl.integer('Project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('Projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
+    .createTable('Resources', tbl => {
+        tbl.increments();
+        tbl.string("ResourcesName")
+        .notNullable();
+        tbl.string("ResourceDescription");
+        tbl.integer('Project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('Projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTableIfExists('Resources')
+  .dropTableIfExists('Tasks')
+  .dropTableIfExists('Projects');
 };
